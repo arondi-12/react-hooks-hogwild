6ba749e8d3726148d80cfc86ad51eff8ffa84ae7
arondi-12 <davieariwi@gmail.com>
2024-11-03 23:46:08 +0300

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
+import './App.css'
 
 import hogs from "../porkers_data";
 
 function App() {
+	const [allHogs, setHogs] = useState(hogs)
+	console.log(allHogs);
+	
 	return (
 		<div className="App">
 			<Nav />
+
+			<div className="hogs">
+				{allHogs.map(hog => (
+					<div className="hog">
+						{hog.name}
+						<img src={hog.image} width={100} height={80} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
